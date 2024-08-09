508027cabc7bd4e88e38f8d5c99b5e08b3228e93
Christopher Hærem <chris.haerem@gmail.com>
2024-08-09 22:29:20 +0200

    chore: Update service worker to cache new icon sizes and fix caching logic

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,44 @@
 const CACHE_NAME = "super-ttt-v1";
 const urlsToCache = [
 	"/",
-	"/index.html",
 	"/css/styles.css",
 	"/js/app.js",
 	"/js/gameLogic.js",
-	"/icon-192x192.png",
-	"/icon-512x512.png",
+	"/images/icon-128x128.png",
+	"/images/icon-256x256.png",
+	"/images/icon-512x512.png",
 ];
 
 self.addEventListener("install", (event) => {
 	event.waitUntil(
-		caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+		caches.open(CACHE_NAME).then((cache) => {
+			return Promise.all(
+				urlsToCache.map((url) => {
+					return cache.add(url).catch((error) => {
+						console.error(`Failed to cache ${url}: ${error}`);
+					});
+				})
+			);
+		})
 	);
 });
 
 self.addEventListener("fetch", (event) => {
 	event.respondWith(
-		caches
-			.match(event.request)
-			.then((response) => response || fetch(event.request))
+		caches.match(event.request).then((response) => {
+			if (response) {
+				return response;
+			}
+			return fetch(event.request).then((response) => {
+				if (!response || response.status !== 200 || response.type !== "basic") {
+					return response;
+				}
+				const responseToCache = response.clone();
+				caches.open(CACHE_NAME).then((cache) => {
+					cache.put(event.request, responseToCache);
+				});
+				return response;
+			});
+		})
 	);
 });
